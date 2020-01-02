7241320c69d090aea9f2bc9ce72ef856e64196fd
Bogdan Levishchev <blevishche@e2r14p10.unit.ua>
2020-01-02 17:08:53 +0200

    11

---
+++ inc/Pathfinder.h
@@ -0,0 +1,12 @@
+#ifndef PATHFINDER_H
+#define PATHFINDER_H
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(argc, argv[]);
